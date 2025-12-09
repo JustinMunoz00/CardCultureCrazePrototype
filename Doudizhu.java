e33f8fb07b82d52258ea92f1d42d1836137bb83c
JustinMunoz00 <justinm880@nycstudents.net>
2025-12-09 22:59:53 +0000

    fixed some problems now just need to add other mechanics and rules.

--- Doudizhu.java
+++ Doudizhu.java
@@ -12,6 +12,8 @@ public class Doudizhu {
             playerHands.add(new ArrayList<Integer>());
         }
         newDeck();
+        System.out.print("Hello players! DouDizhu essentially a chinese version of poker. It is a 3 player game where 1 player is the landowner who the other 2 will compete against. \nThe landowner starts out with 3 extra cards that they have to reveal to the other 2 players. The goal of all players is to use all their cards. If either the land ownner ");
+        System.out.println("or 1 of the other 2 players reach this goal the game ends. Only 1 of the 2 players are required to win for both to win. The landowner always starts first and can choose how they want to play (single, double, triple, straight). \nAll other players have to place the same type as the land owner (if the landowner plays doubles they have to play doubles). If you are the last person to put down a card then you restart the pile and can play whatever you would like. ");
     }
 
     private void newDeck(){
@@ -43,27 +45,50 @@ public class Doudizhu {
 
     public void deal(){
         for (int i = 0; i < 3; i++){
-            int n = (int) Math.random()*(54-i);
+            int n = (int) (Math.random()*(54-i));
             landlordCards[i] = deck.get(n);
             deck.remove(n);
         }
         for (int i = 0; i < 17; i++){
             for (int n = 0; n < 3; n++){
-                int x = (int) Math.random()*(deck.size());
-                playerHands.get(n).add(x);
+                int x = (int) (Math.random()*(deck.size()));
+                int card = deck.get(x);
+                playerHands.get(n).add(card);
                 deck.remove(x);
             }
         }
-        landLord = (int) Math.random()*3 + 1;
+        landLord = (int) (Math.random()*3) + 1;
         System.out.println("Player " + landLord + " is the landlord!");
+        System.out.print("The three cards were: ");
+        for (int i : landlordCards){
+            String card = cardValue.get(i);
+            System.out.print(card + " ");
+        }
+        System.out.println();
         for (int i = 0; i < 3; i++){
             playerHands.get(landLord-1).add(landlordCards[i]);
         }
     }
 
+    private void sortHands(){
+        for (int m = 0; m < 3; m++){
+            ArrayList<Integer> currentHand = playerHands.get(m);
+            int n = currentHand.size();
+            for (int i = 0; i < n; ++i) {
+                int key = currentHand.get(i);
+                int j = i - 1;
+            while (j >= 0 && currentHand.get(j) > key) {
+                currentHand.set(j+1, currentHand.get(j));
+                j = j - 1;
+            }
+            currentHand.set(j+1, key);
+            }
+        }
+    }
     public void showHands(){
+        sortHands();
         for (int player = 0; player < 3; player++){
-            System.out.print("Player 1: ");
+            System.out.print("Player " + (player+1) + ": ");
             for (int i = 0; i < playerHands.get(player).size(); i++){
                 String card = cardValue.get(playerHands.get(player).get(i));
                 System.out.print(card + ", ");
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         Doudizhu game = new Doudizhu();
         game.deal();
         game.showHands();
+        s.close();
     }
 }
